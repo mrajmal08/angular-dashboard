e27d97d1c0d16edbfb6ee3d0d34fe87e6638144d
Mr Ajmal <ajmalg08@gmail.com>
2022-04-08 12:59:07 +0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(
+      {
+        timeOut: 2000,
+        progressBar: true,
+        progressAnimation: 'increasing',
+        preventDuplicates: true,
+      }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,95 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { AuthServiceService } from '../auth-service.service';
+
+import { ToastrService } from 'ngx-toastr';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  currentYear: number = new Date().getFullYear();
+
+  formGroup: FormGroup = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  submitted = false;
+  message: boolean = false;
+
+  constructor(
+    private authSerivce: AuthServiceService,
+    private formBuilder: FormBuilder,
+    private toastr: ToastrService
+  ) {}
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm() {
+    // this.formGroup = this.formBuilder.group(
+    //   {
+    //     email: ['',
+    //      [
+    //        Validators.required,
+    //        Validators.email
+    //       ]],
+    //     password: [
+    //       '',
+    //       [
+    //         Validators.required,
+    //         Validators.minLength(6),
+    //         Validators.maxLength(40)
+    //       ]
+    //     ],
+    //   },
+    // );
+  }
+
+  showDanger(message:any) {
+    this.toastr.warning('Validation Error!', message);
+  }
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.formGroup.controls;
+  }
+
+  loginProcess(): void {
+    this.submitted = true;
+    if (this.formGroup.invalid) {
+      setTimeout(() => {
+        this.submitted = false;
+        this.formGroup.reset();
+      }, 2000);
+
+      return;
+    }
+    console.log(JSON.stringify(this.formGroup.value, null, 2));
+
+    this.authSerivce.login(this.formGroup.value).subscribe(
+      (res) => {
+        let data = res;
+        if (data.status === true) {
+          console.log(data.data.token);
+          alert(data.message);
+        }
+      },
+      (error) => {
+        if(error.status == 401){
+          // this.message = true;
+          this.showDanger('invalid email or password');
+        };
+      }
+    );
+  }
+}
